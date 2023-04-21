1e9afb6080fb3595204a9f61bd434c9c9e202f3e
SHUBAAA <m.alvial03@ufromail.cl>
2023-04-21 18:10:48 -0400

    Modificado final

--- tarea2.js
+++ tarea2.js
@@ -1,26 +1,34 @@
-const jugador1={
+const jugador1 = {
     nombre: "jugador1",
-    vidamaxima:1000,
-    vidaactual:1000,
-    dano:500
+    vidamaxima: 1000,
+    vidaactual: 1000,
+    dano: 200
 };
 
-const jugador2={
+const jugador2 = {
     nombre: "jugador2",
-    vidamaxima:1000,
-    vidaactual:1000,
-    dano:200
+    vidamaxima: 1000,
+    vidaactual: 1000,
+    dano: 500
 };
 
 console.log("Empieza el combate");
+console.log("La vida del jugador 1 es " + jugador1.vidaactual +"/"+jugador1.vidamaxima+" HP")
+console.log("La vida del jugador 2 es " + jugador2.vidaactual+"/"+jugador2.vidamaxima+" HP")
+do {
+console.log("Ronda"+(0+1))
+    console.log("El jugador 2 hace " + jugador2.dano + " de daño")
+    jugador1.vidaactual = jugador1.vidaactual - jugador2.dano
+    console.log("La vida del jugador 1 es " + jugador1.vidaactual +"/"+jugador1.vidamaxima+" HP")
+
+    console.log("El jugador 1 hace " + jugador1.dano+ " de daño")
+    jugador2.vidaactual = jugador2.vidaactual - jugador1.dano
+    console.log("La vida del jugador 2 es " + jugador2.vidaactual+"/"+jugador2.vidamaxima+" HP")
+} while (jugador1.vidaactual > 0 && jugador2.vidaactual > 0)
 
 
-
-do{
-    jugador1.vidaactual=jugador1.vidaactual-jugador2.dano
-   console.log("la vida del jugador 1 es "+ jugador1.vidaactual);
-
-
-   jugador2.vidaactual=jugador2.vidaactual-jugador1.dano
-   console.log("la vida del jugador 2 es "+jugador2.vidaactual)
-}while(jugador1.vidaactual>0&& jugador2.vidaactual>0)
+if(jugador1.vidaactual<=0){
+    console.log("Se murio el jugador 1")
+}else{
+    console.log("Se murio el jugador 2")
+}

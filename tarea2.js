c2c39714e2a4d74b86a5324818c86249668307c6
SHUBAAA <m.alvial03@ufromail.cl>
2023-04-21 15:47:38 -0400

    modificado 2

--- tarea2.js
+++ tarea2.js
@@ -9,7 +9,7 @@ const jugador2={
     nombre: "jugador2",
     vidamaxima:1000,
     vidaactual:1000,
-    dano:500
+    dano:200
 };
 
 console.log("Empieza el combate");
@@ -23,4 +23,4 @@ do{
 
    jugador2.vidaactual=jugador2.vidaactual-jugador1.dano
    console.log("la vida del jugador 2 es "+jugador2.vidaactual)
-}while(jugador1.vidaactual!=0|| jugador2.vidaactual!=0)
+}while(jugador1.vidaactual>0&& jugador2.vidaactual>0)
